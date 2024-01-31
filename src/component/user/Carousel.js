c18b1ea55fbd047dac4a5e92a2836cbde641861f
Bhuwan Shrestha <bhuwan.crestha57@gmail.com>
2024-01-31 14:49:11 +0545

    change

--- src/component/user/Carousel.js
+++ src/component/user/Carousel.js
@@ -22,16 +22,28 @@ const Carousel = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" />
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            alt="example"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
+          <img
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+            alt="example"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" />
+          <img
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+            alt="example"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" />
+          <img
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+            alt="example"
+          />
         </SwiperSlide>
       </Swiper>
     </>
