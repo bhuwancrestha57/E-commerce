572da577cd05f7893cfd3e55449271a0fa467102
Bhuwan Shrestha <bhuwan.crestha57@gmail.com>
2024-02-03 18:36:16 +0545

    change

--- src/component/user/cart.js
+++ src/component/user/cart.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import { Avatar, Badge, Drawer } from "antd";
-import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Avatar, Badge, Card, Drawer, Rate } from "antd";
+import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { useAppContext } from "../../ContextApi";
+
 const Cart = () => {
+  const { appState } = useAppContext();
   const [open, setOpen] = useState(false);
+
   const showDrawer = () => {
     setOpen(true);
   };
+
   const onClose = () => {
     setOpen(false);
   };
+
   return (
     <>
       <Badge onClick={showDrawer} count={1}>
@@ -16,11 +22,35 @@ const Cart = () => {
       </Badge>
 
       <Drawer title="Your Cart items" onClose={onClose} open={open}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div>
+          <Card
+            hoverable
+            style={{
+              width: 50,
+              height: 50,
+            }}
+            cover={<img alt="example" src={appState.detail.image} />}
+          ></Card>
+        </div>
+        <div>
+          <div className="text-xl font-bold">{appState.detail.name}</div>
+          <div>Price:{appState.detail.price}</div>
+          <div>Brand:{appState.detail.brand}</div>
+
+          <div>
+            <Rate value={appState.detail.rating} />
+          </div>
+
+          <div>
+            <EyeOutlined />
+            View:{appState.detail.view}
+          </div>
+          <div>Stock Available:{appState.detail.stockItem}</div>
+          <div>Description:{appState.detail.description}</div>
+        </div>
       </Drawer>
     </>
   );
 };
+
 export default Cart;
